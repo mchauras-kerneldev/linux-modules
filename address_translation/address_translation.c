3cf20af6f5a1be4853057d897be579d6ad884203
root <root@ltczz256-lp1.aus.stglabs.ibm.com>
2024-02-08 01:34:01 -0500

    Printing Addresses
    Signed-off-by: root <root@ltczz256-lp1.aus.stglabs.ibm.com>

--- address_translation/address_translation.c
+++ address_translation/address_translation.c
@@ -4,17 +4,23 @@
 #include <linux/module.h>
 #include <asm/io.h>
 
-static int __init address_translation_init(void) {
+static void print_address(void* addr, char* name) {
   phys_addr_t phys_addr;
-  pr_info("Address Translation Module Loaded\n");
-  phys_addr = virt_to_phys((void *)&address_translation_init);
+  phys_addr = virt_to_phys(addr);
+  pr_info("************* Symbol %s *************\n", name);
   pr_info("Physical Address: 0x%llx\n", (unsigned long long) phys_addr);
-  pr_info("Virtual Address: 0x%llx\n", &address_translation_init);
-  return 0;
+  pr_info("Virtual Address: 0x%llx\n", (unsigned long long) addr);
 }
 
 static void __exit address_translation_exit(void) {
   pr_info("Address Translation Module Unloaded\n");
+}
+
+static int __init address_translation_init(void) {
+  pr_info("Address Translation Module Loaded\n");
+  print_address((void *) &address_translation_init, "address_translation_init");
+  print_address((void *) &address_translation_exit, "address_translation_exit");
+  return 0;
 }
 
 module_init(address_translation_init);

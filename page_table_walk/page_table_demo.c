11bd3cb19955fa062b864f59dc3365c2d7dcdc59
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2024-04-17 17:47:25 +0530

    Read logic updated to read the data from the saved memory data
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- page_table_walk/page_table_demo.c
+++ page_table_walk/page_table_demo.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-	int a=8055;
-	printf("%p\n",&a);
-	while(1) {
-		sleep(10000);
-	}
+#define ARR_SIZE 128 * 1024 * 1024
+
+int main() {
+  int a = 0;
+  char *s = "Mukesh Kumar Chaursiya";
+  char *temp = (char *)malloc(ARR_SIZE);
+  printf("Integer Value address %p\n", &a);
+  printf("Char Pointer address %p with len %ld\n", s, strlen(s));
+  printf("Address of large char pointer start: %p\tend: %p\n", temp,
+         temp + (ARR_SIZE)-1);
+  memset(temp, 0, ARR_SIZE);
+  while (1) {
+    temp[a] = a;
+    if (a >= ARR_SIZE) {
+      a = 0;
+    }
+		a++;
+    sleep(1000);
+  }
 }
